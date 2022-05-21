6f1a857b1f01abfe018512a2f00d2b8471c30675
Charles Cailleteau <charles.cailleteau@protonmail.com>
2022-05-21 16:23:17 +0200

    path viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,12 @@ import { defineConfig } from 'vite'
 export default defineConfig({
 	build: {
 		outDir: './docs',
+		rollupOptions: {
+			input: {
+				main: 'index.html',
+				magazine: './magazine/index.html',
+			},
+		},
 	},
 	base: '/dnofa/',
 })
