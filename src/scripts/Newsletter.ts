2ea445acf4574a2be944d0d4b0d4f3eed06ab4e5
Charles Cailleteau <charles.cailleteau@protonmail.com>
2022-05-29 12:23:41 +0200

    fix :content, newsletter endpoint

--- src/scripts/Newsletter.ts
+++ src/scripts/Newsletter.ts
@@ -2,7 +2,7 @@ import MicroModal from 'micromodal'
 
 export default class Newsletter {
 	static endPoint =
-		'https://0b252bd2.sibforms.com/serve/MUIEAPETmDG0I0c7TpXMSu8FGnlhjbEBdXnKWOHiZ5ONq44yhGToZkR0UIJs467qo9JWDPF4szw0CuiuHzOaBBwmoi7Lh4Ybd9KL9fvZ86bNDQVJX8igXwbRFlPF4Q3xevkV5RJpVL1jCUtcx-VnGKv82Ffl-Gi-QPficBqNoWroLyUSTcJYkKsdj-MDpVmaC0JK_NO9g3ZfwiV4?isAjax=1'
+		'https://8bb72497.sibforms.com/serve/MUIEADyvPYf60YflDxW4RB4NxMQsmiYIZSrrZFweX_cTa8jjt5VYrlGygJQq4hjT923GQpiF601Ho8OMQbZkUfr18giW8EljUSn7ccl5iGmwiibg36DGYgTvCUojL1ztxzNO9ZzgEtgoIZBqn8Avbya2MbxZfX2LxSyahXnhGj7l9MGMMP_pxyKYExzzCMQGmzTin8jSLC_TVx7T?isAjax=1'
 
 	static init() {
 		const form = document.querySelector<HTMLFormElement>('#newsletter-js')
