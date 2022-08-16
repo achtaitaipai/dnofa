00eaa1bc5b0be97766d750cbbef2168df0ee0e94
achtaitaipai <charles.cailleteau@gmail.com>
2022-08-16 16:07:04 +0200

    viewer pages

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,17 @@ import './style.scss'
 import Menu from './scripts/Menu'
 import ScrollAnims from './scripts/ScrollAnims'
 import Newsletter from './scripts/Newsletter'
+import ImagesViewer from './scripts/Viewer'
+// import { tns } from 'tiny-slider'
 Menu.init()
 ScrollAnims.init()
 Newsletter.init()
+ImagesViewer.init()
+// tns({
+//     "container": "#slider",
+//     "items": 3,
+//     "slideBy": "page",
+//     "mouseDrag": true,
+//     "swipeAngle": false,
+//     "speed": 400
+//   })
