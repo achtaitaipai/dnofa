ad57ab9380aa9dc69184fd700fb35d05eed0e131
achtaitaipai <charles.cailleteau@gmail.com>
2022-08-19 09:10:49 +0200

    viewer page magazine

--- src/scripts/Viewer.ts
+++ src/scripts/Viewer.ts
@@ -1,38 +1,37 @@
-import Viewer from "viewerjs";
-import "viewerjs/dist/viewer.css";
+import Viewer from 'viewerjs'
+import 'viewerjs/dist/viewer.css'
 
 export default class ImagesViewer {
-    private static _galleryElement:HTMLUListElement|null
-    private static _imgs:HTMLImageElement[]
+	private static _galleryElement: HTMLUListElement | null
+	private static _imgs: HTMLImageElement[]
 
-  static init() {
-    this._galleryElement = document.querySelector<HTMLUListElement>('#images-gallery-js')
-    this._imgs=Array.from(document.querySelectorAll<HTMLImageElement>('[data-gallery]'))
-    if(this._galleryElement){
-        const gallery = new Viewer(this._galleryElement,{
-            inline:false,
-            title:false,
-            toolbar:{
-                "zoomIn":true, 
-                "zoomOut":true,
-                "oneToOne":false,
-                "reset":false,
-                "prev":false,
-                "play":false,
-                "next":false,
-                "rotateLeft":false,
-                "rotateRight":false,
-                "flipHorizontal":false,
-                "flipVertical":false
-            }
-        });
-        this._imgs.forEach(img=>{
-            const imgNumber=parseInt(img.getAttribute('data-gallery')??'0')
-            console.log(imgNumber)
-            img.addEventListener('click',()=>{
-                gallery.view(imgNumber)
-            })
-        })
-    }
-  }
+	static init() {
+		this._galleryElement = document.querySelector<HTMLUListElement>('#images-gallery-js')
+		this._imgs = Array.from(document.querySelectorAll<HTMLImageElement>('[data-gallery]'))
+		if (this._galleryElement) {
+			const gallery = new Viewer(this._galleryElement, {
+				inline: false,
+				title: false,
+				toolbar: {
+					zoomIn: true,
+					zoomOut: true,
+					oneToOne: false,
+					reset: false,
+					prev: false,
+					play: false,
+					next: false,
+					rotateLeft: false,
+					rotateRight: false,
+					flipHorizontal: false,
+					flipVertical: false,
+				},
+			})
+			this._imgs.forEach(img => {
+				const imgNumber = parseInt(img.getAttribute('data-gallery') ?? '0')
+				img.addEventListener('click', () => {
+					gallery.view(imgNumber)
+				})
+			})
+		}
+	}
 }
